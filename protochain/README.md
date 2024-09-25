574290a3f86b24994b1be38d4d7c1ccf776891da
Bernard Mendes <bernard.hazlehurst@crea-rj.org.br>
2024-09-25 15:24:52 -0300

    protochain v0.1

---
+++ protochain/README.md
@@ -0,0 +1,2 @@
+# protochain
+Module 1. A proto-blockchain project.
---
+++ protochain/__tests__/blockchain.test.ts
@@ -0,0 +1,47 @@
+import { describe, test, expect } from '@jest/globals';
+import Block from '../src/lib/block';
+import Blockchain from '../src/lib/blockchain';
+
+describe("Blockchain tests", () => {
+    test('Should have genesis block', () => {
+        const blockchain = new Blockchain();
+        expect(blockchain.blocks.length).toEqual(1);
+    })
+
+    test('Should be valid (genesis)', () => {
+        const blockchain = new Blockchain();        
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should be valid (two blocks)', () => {
+        const blockchain = new Blockchain();        
+        blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, "Block 2"));
+        expect(blockchain.isValid().success).toEqual(true);
+    })
+
+    test('Should NOT be valid', () => {
+        const blockchain = new Blockchain();        
+        blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, "Block 2"));
+        blockchain.blocks[1].data = "a transfere 2 para b";
+        expect(blockchain.isValid().success).toEqual(false);
+    })
+
+    test('Should add block', () => {
+        const blockchain = new Blockchain();        
+        const result = blockchain.addBlock(new Block(1, blockchain.blocks[0].hash, "Block 2"));
+        expect(result.success).toEqual(true);
+    })
+
+    test('Should get block', () => {
+        const blockchain = new Blockchain();        
+        const block = blockchain.getBlock(blockchain.blocks[0].hash);
+        expect(block).toBeTruthy();
+    })
+
+    test('Should NOT add block', () => {
+        const blockchain = new Blockchain();    
+        const block = new Block(-1, blockchain.blocks[0].hash, "Block 2");
+        const result = blockchain.addBlock(block);
+        expect(result.success).toEqual(false);
+    })
+})
